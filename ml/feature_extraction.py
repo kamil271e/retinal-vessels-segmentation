e2e2d6ae6ff966ab4a5e58fcd42923e11cd3c4cf
kamil <kamil271e@gmail.com>
2023-05-27 13:16:26 +0200

    [#2] Code refactor and better performance on unseen data

--- ml/feature_extraction.py
+++ ml/feature_extraction.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
 
-# image section -> vector of features
+num_features = 13
+
 def extract_features(img):
     means, stds = cv2.meanStdDev(img)
     stats = np.concatenate([means, stds], axis=0).flatten()
@@ -10,3 +11,10 @@ def extract_features(img):
     feature_vect = np.concatenate((stats, hu_moments), axis=0)
     return feature_vect
 
+def convert_to_feature_vec(sections):
+    section_features = np.zeros((sections.shape[0], num_features))
+
+    for i in range(sections.shape[0]):
+        section_features[i] = extract_features(sections[i])
+    
+    return section_features
--- ml/preprocess.py
+++ ml/preprocess.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-
 def clahe(image, clip_limit=2.0, grid_size=(8, 8)):
     lab_image = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
     l_channel, a_channel, b_channel = cv2.split(lab_image)
@@ -21,43 +20,18 @@ def hist_equalization(img):
     return equalized_img
     
     
-def padding(img, n, rgb=True):
-    if rgb:
-        rows, cols = img[:,:,0].shape
-    else:
-        rows, cols = img.shape
-        
-    add_rows, add_cols = n - (rows % n), n - (cols % n)
-    append_columns = np.zeros((rows, add_cols), dtype=img.dtype)
-    append_rows = np.zeros((add_rows, cols + add_cols), dtype=img.dtype)
-    
-    if rgb:
-        channels = np.zeros((rows+add_rows, cols+add_cols, 3))
-        for i in range(3):
-            channel = img[:,:,i]
-            channel = np.concatenate((channel, append_columns), axis=1)
-            channel = np.concatenate((channel, append_rows), axis=0)
-            channels[:,:,i] = channel   
-        img = np.stack([c for c in channels], axis=0).astype(np.float32)    
-    
-    else:
-        img = np.concatenate((img, append_columns), axis=1)
-        img = np.concatenate((img, append_rows), axis=0)
-    
-    return img / 255.0
-
-
 # we move window by one pixel each time
 # if passed image is size MxM this function will return M*M sections of size N*N
 # each section correspond to one pixel in the input image
-def sectioning_valid(img, n):   
+def sectioning_img(img, n):   
     if n % 2 == 0:
         print('N should be an odd number')
         return
     
-    # TODO perform padding similar like in 'sectioning' function    
+    img = clahe(img)
+    
+    # TODO perform padding similar like in unused 'sectioning' function    
     rows, cols = img.shape[0], img.shape[1]
-    print(rows, cols)
     num_sections = rows * cols
     img_sections = np.zeros((num_sections, n, n, 3))
         
@@ -72,52 +46,18 @@ def sectioning_valid(img, n):
     return img_sections
     
     
-# cool but not used in proper solution
-def display_sections(img_sections, img_size, n): # test for low resolutions or/and big windows: e.g 100x100, 10x10
-    if img_sections.ndim != 3 and img_sections.ndim != 4:
-        print('Display available for 2D or 3D sections')
-        return
-    
-    if img_sections.ndim == 3:
-        rows, cols = img_size # padded img_size !
-    else:
-        rows, cols = img_size[0], img_size[1]
-        
-    rows = int(rows / n)
-    cols = int(cols / n)
-    
-    fig, ax = plt.subplots(rows, cols, figsize=(4,4))
-    for i in range(rows):
-        for j in range(cols):
-            ax[i, j].axis('off')
-            ax[i, j].imshow(img_sections[cols * i + j].astype(np.uint8))
-    plt.subplots_adjust(wspace=0.05, hspace=0.05)
-    plt.show()
-    
-    
-# cool but not used in proper solution
-def sectioning(img, target, n, debug=False):
-    # n x n sections TODO
-    if img.shape[0] % n != 0 or img.shape[1] % n != 0:
-        img = padding(img, n)
-        
-    if target.shape[0] % n != 0 or target.shape[1] % n != 0:
-        target = padding(target, n, rgb=False)
-    
-    if debug:
-        print('Img padded shape: ', img.shape)
-        print('Target padded shape: ', target.shape)
-    
-    rows, cols = int(img.shape[0] / n) , int(img.shape[1] / n)
-    num_sections = rows * cols
-    img_sections = np.zeros((num_sections, n, n, 3))
-    target_sections = np.zeros((num_sections, n, n))
+# sectioning num_img nuber of randomly sampled images  
+def sectioning(X, y, num_img, img_size, n):
+    sections = np.zeros((num_img, img_size[0]*img_size[1], n, n, 3))
+    targets = np.zeros((num_img, img_size[0]*img_size[1]))
+    random_imgs_indx = [np.random.randint(0, len(X)-1) for _ in range(num_img)]
 
-    for i in range(rows):
-        for j in range(cols):
-            img_sections[i * cols + j] = img[i * n : (i+1) * n, j * n : (j+1) * n]
-            target_sections[i * cols + j] = target[i * n : (i+1) * n, j * n : (j+1) * n]
-            
-    return img_sections, target_sections, img.shape, target.shape
+    for i, indx in enumerate(random_imgs_indx):
+        sections[i] = sectioning_img(X[indx], n)
+        _, target = cv2.threshold(y[indx], 128, 1, cv2.THRESH_BINARY)
+        targets[i] = target.flatten()
+
+    sections = sections.reshape(num_img*img_size[0]*img_size[1], n, n, 3)
+    targets = targets.flatten()
     
- 
+    return sections, targets
